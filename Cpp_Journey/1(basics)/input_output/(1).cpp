35162c9321edc751ee869fd04d5432cb47ee252f
Daniel Veiga <daveiga.office@gmail.com>
2025-07-30 18:55:08 -0300

    (1)

--- Cpp_Journey/1(basics)/input_output/(1).cpp
+++ Cpp_Journey/1(basics)/input_output/(1).cpp
@@ -1,1 +1,22 @@
-assdadas
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main () {
+
+    std::string name;
+    int age;
+
+    std::cout << "Your name: ";
+    std::getline(std::cin, name);
+
+    std::cout << std::endl;
+    
+    std::cout << "Your age:";
+    std::cin >> age; 
+
+    std::cout << "Name: " << name
+    << std::endl;
+    std::cout << "Age: " << age;
+
+}
